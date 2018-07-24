3527aa19cb752a4a4812d0d239a592fd93c17ab6
cindywang222 <cindywang.222@gmail.com>
2018-07-24 10:29:03 -0700

    Add files

---
+++ BART-Alarm/BART-Alarm/Models/Trip.swift
@@ -0,0 +1,16 @@
+//
+//  Trip.swift
+//  BART-Alarm
+//
+//  Created by Cindy Wang on 7/24/18.
+//  Copyright © 2018 cxw. All rights reserved.
+//
+
+import UIKit
+
+struct Trip {
+    var startLocation
+    var endLocation
+    var travelTime
+    var alarmTime
+}
