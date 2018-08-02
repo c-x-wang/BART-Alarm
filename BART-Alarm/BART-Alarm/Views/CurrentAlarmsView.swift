c7774fcdeb142e11b3a388d4ed10c038f9791c61
cindywang222 <cindywang.222@gmail.com>
2018-08-02 11:33:14 -0700

    Revert "Revert "Begin adding options to open up alarms?""
    This reverts commit 12da835253ac1d3bfab2d6147eda32c53f649dae.

---
+++ BART-Alarm/BART-Alarm/Views/CurrentAlarmsView.swift
@@ -0,0 +1,26 @@
+//
+//  CurrentAlarmsView.swift
+//  BART-Alarm
+//
+//  Created by Cindy Wang on 8/2/18.
+//  Copyright © 2018 cxw. All rights reserved.
+//
+
+import UIKit
+
+class CurrentAlarmsView: UIView {
+    
+    @IBOutlet weak var routeNameLabel: UILabel!
+    @IBOutlet weak var routeStationsLabel: UILabel!
+    @IBOutlet weak var trainDepartureTimeLabel: UILabel!
+    @IBOutlet weak var alarmTimeLabel: UILabel!
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
